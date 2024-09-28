8601046ead445a0f2e2bad8c8ddccc7181dbdb6f
Dusan Antonijevic <dusan.antonijevic2404@gmail.com>
2024-09-28 17:45:56 +0200

    Update descriptions and change names of commands

--- src/Commands/DeleteAllKeysCommand.php
+++ src/Commands/DeleteAllKeysCommand.php
@@ -7,7 +7,9 @@ use LaravelTolgee\Services\TolgeeService;
 
 class DeleteAllKeysCommand extends Command
 {
-    protected $signature = 'tolgee:delete-all-keys';
+    protected $signature = 'tolgee:keys:flush';
+
+    protected $description = 'Command will delete all keys in Tolgee project.';
 
     public function __construct(private readonly TolgeeService $service)
     {
--- src/Commands/ImportKeysCommand.php
+++ src/Commands/ImportKeysCommand.php
@@ -7,7 +7,9 @@ use LaravelTolgee\Services\TolgeeService;
 
 class ImportKeysCommand extends Command
 {
-    protected $signature = 'tolgee:import-keys';
+    protected $signature = 'tolgee:keys:sync';
+
+    protected $description = 'Command will sync all keys from local project files to Tolgee.This will not overwrite existing keys.';
 
     public function __construct(private readonly TolgeeService $service)
     {
